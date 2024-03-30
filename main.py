f7df874a4e577654de89068c03c6440e3e7bf8eb
AansRehman <aansrehman10@gmail.com>
2024-03-30 15:25:34 +0500

    changes in main.py

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load the trained model from the pickle file
 with open('diabetes_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-# Define the function to predict diabetes
 def predict_diabetes(row):
     prediction = model.predict([row])
     return 'Positive' if prediction[0] == 1 else 'Negative'
 
-# Define the Flask endpoint
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
